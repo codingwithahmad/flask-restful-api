5d138f9d7bf39bd0cdacd32b13a60838dc5991ef
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-10 18:46:55 +0330

    set aplication env and database

---
+++ todo/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+	app = Flask(__name__)
+
+	app.config.from_mapping(
+		SECRET_KEY=os.environ.get('SECRET_KEY'),
+		SQLALCHEMY_TRACK_MODIFICATIONS=False,
+		SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+	)	
+
+	db.init_app(app)
+
+	from . import main 
+	app.register_blueprint(main.main)
+
+	return app
